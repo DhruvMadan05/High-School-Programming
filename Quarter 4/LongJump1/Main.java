833fb66f736be0e482f52736b4ae449e871efdee
DhruvMadan05 <63520379+DhruvMadan05@users.noreply.github.com>
2021-04-16 12:34:46 -0400

    commit

--- Quarter 4/LongJump1/Main.java
+++ Quarter 4/LongJump1/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-
+		
 		LongJumpAthlete athlete = new LongJumpAthlete();
 		// Use Scanner to read from the file
 
